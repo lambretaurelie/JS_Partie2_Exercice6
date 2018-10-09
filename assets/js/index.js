9cf570d1864744545e9e381afae72a774cbbd0a0
lambret <aurelie.l-02@hotmail.fr>
2018-10-09 12:30:18 +0200

    exercice 6

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function changeImages(image){
+  document.getElementById(image).src='assets/images/'+image+'_2.jpg';
+}
+function changeAgain(image){
+  document.getElementById(image).src='assets/images/'+image+'.jpg';
+}
